24ae6156ab6a565ed5dfbad09b1e6b263fd8c9b1
carvallegro <valentin.carmignac@gmail.com>
2016-01-15 23:27:53 +0100

    created my own icon font using fontastic.me Adapted and used SASS code from FontAwesome Imported HoverCSS SASS code (yet to be adapted) Finalized menu

---
+++ app/scripts/controllers/uicontroller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myTumblrApp.controller:UicontrollerCtrl
+ * @description
+ * # UicontrollerCtrl
+ * Controller of the myTumblrApp
+ */
+angular.module('myTumblrApp').controller('UicontrollerCtrl',[ '$scope', function ($scope) {
+    this.isMenuOpen=$scope.isMenuOpen=true;
+  }]);
